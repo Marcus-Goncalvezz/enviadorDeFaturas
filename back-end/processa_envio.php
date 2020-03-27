0a03e3119c2e5858e77e3fdaf91caa00b15502c7
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-03-27 03:04:53 -0300

    Ajustes

--- back-end/processa_envio.php
+++ back-end/processa_envio.php
@@ -101,12 +101,12 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>App Mail Send</title>
+    <title>Enviar Faturas</title>
 </head>
 <body>
     <div class="container">
         <div class="py-3 text-center">
-				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<img class="d-block mx-auto mb-2" src="logo-master.png" alt="" width="72" height="72">
 				<h2>Enviar Faturamento</h2>
 			</div>
         <div class="row">
